0da41cba67177e48be4ad9a85d29caacd6e6884a
ANAMIKA VISH <av.anamika1@gmail.com>
2025-08-12 10:33:22 +0530

    update some chamges

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,19 +3,25 @@ class HomeController < ApplicationController
 
   def index
   end
-  
+
   def dashboard
-    case current_user.role
-    when 'employee'
-      redirect_to employee_details_path
-    when 'hod'
-      redirect_to new_user_detail_path
-    when 'l1_employer'
-      redirect_to l1_employee_details_path
-    when 'l2_employer'
-      redirect_to l2_employee_details_path
-    else
-      render plain: "No dashboard for this role"
+    if current_user.hod?
+      redirect_to employee_details_path and return
     end
+
+    if EmployeeDetail.exists?(l2_code: current_user.employee_code) || EmployeeDetail.exists?(l2_employer_name: current_user.email)
+      redirect_to l2_employee_details_path and return
+    end
+
+    if EmployeeDetail.exists?(l1_code: current_user.employee_code)
+      redirect_to l1_employee_details_path and return
+    end
+
+    if current_user.employee?
+      redirect_to get_user_detail_user_details_path and return
+    end
+
+    # fallback if none of above
+    render plain: "No dashboard or redirect for your role."
   end
 end
